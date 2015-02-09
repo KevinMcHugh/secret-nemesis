972ab1d2f46dc33d1a844543dbd769e80375ba8a
Kevin McHugh <kev@kevinmchugh.me>
2015-02-08 22:24:24 -0600

    LEss complex method in Mission

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -11,6 +11,7 @@ class Mission
 
   def play
     votes_failed = 0
+    # TODO: team size
     team = leader.pick_team
     vote_passes = vote(team)
     while !vote_passes
@@ -24,17 +25,7 @@ class Mission
       vote_passes = vote(team)
     end
     if vote_passes
-      spies = team.find_all { |p| p.spy? }
-      votes = spies.map { |p| p.pass_mission?(team) }
-      votes += (team.length - spies.length).times.map {true}
-      players.each { |p| p.show_mission_plays(votes.group_by {|o| o })}
-      if votes.include?(false)
-        # TODO mission 4 in 7+ player games
-        # TODO eventing makes printing possible :/
-        @winning_team = 'spy'
-      else
-        @winning_team = 'resistance'
-      end
+      mission(team)
     end
   end
 
@@ -50,5 +41,19 @@ class Mission
     disapprove_votes = grouped_votes[false].try(:count) || 0
     players.each { |p| p.show_player_votes(players_to_votes) }
     approve_votes > disapprove_votes
+  end
+
+  def mission(team)
+    spies = team.find_all { |p| p.spy? }
+    votes = spies.map { |p| p.pass_mission?(team) }
+    votes += (team.length - spies.length).times.map {true}
+    players.each { |p| p.show_mission_plays(votes.group_by {|o| o })}
+    if votes.include?(false)
+      # TODO mission 4 in 7+ player games
+      # TODO eventing makes printing possible :/
+      @winning_team = 'spy'
+    else
+      @winning_team = 'resistance'
+    end
   end
 end
