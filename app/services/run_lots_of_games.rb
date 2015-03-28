c645ee6fda54354c9f4a7ebcce81c507d7957762
Kevin McHugh <kev@kevinmchugh.me>
2015-03-28 10:48:41 -0500

    mega_game_runner runs games for 5-10 players
    5
    [[["resistance"], 296], [["spy"], 704]]
    6
    [[["resistance"], 330], [["spy"], 670]]
    7
    [[["resistance"], 366], [["spy"], 634]]
    8
    [[["resistance"], 304], [["spy"], 696]]
    9
    [[["resistance"], 468], [["spy"], 532]]
    10
    [[["resistance"], 297], [["spy"], 703]]

---
+++ app/services/run_lots_of_games.rb
@@ -0,0 +1,30 @@
+class RunLotsOfGames
+
+  attr_reader :number_of_games, :number_of_players
+  def initialize(number_of_games: 1000, number_of_players: 5)
+    @number_of_games   = number_of_games
+    @number_of_players = number_of_players
+  end
+
+
+  def perform
+    games = number_of_games.times.flat_map do |i|
+      brain_classes = []
+      # brain_classes += 2.times.map {VeryNegativeBrain}
+      # brain_classes += 2.times.map {CrazyBrain}
+      # brain_classes += 2.times.map {VeryPositiveBrain}
+      brain_classes += number_of_players.times.map {GoodBrain}
+
+      game = Game.new(brain_classes)
+      game.play
+      game
+    end
+
+    winners = games.map {|game| game.winners.map(&:role).uniq }
+    role_counter = Hash.new(0)
+    winners.reduce(role_counter){ |h, e| h[e] += 1 ; h }
+
+    role_counter.sort_by {|key, value| value}
+  end
+
+end
--- mega_game_runner.rb
+++ mega_game_runner.rb
@@ -1,17 +1,5 @@
-games = 10000.times.flat_map do |i|
-  brain_classes = []
-  # brain_classes += 2.times.map {VeryNegativeBrain}
-  # brain_classes += 2.times.map {CrazyBrain}
-  # brain_classes += 2.times.map {VeryPositiveBrain}
-  brain_classes += 5.times.map {GoodBrain}
-
-  game = Game.new(brain_classes)
-  game.play
-  game
+(5..10).each do |i|
+  results = RunLotsOfGames.new(number_of_players: i).perform
+  puts i
+  pp results
 end
-
-winners = games.map {|game| game.winners.map(&:role).uniq }
-role_counter = Hash.new(0)
-winners.reduce(role_counter){ |h, e| h[e] += 1 ; h }
-
-pp role_counter.sort_by {|key, value| value}
